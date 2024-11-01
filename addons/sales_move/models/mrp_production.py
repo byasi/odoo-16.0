7719374d5accdf061c2badfa2abd5ad73f28e9fe
byasi <solomonbyasi@gmail.com>
2024-11-01 18:34:17 +0300

    bug fixes

--- addons/sales_move/models/mrp_production.py
+++ addons/sales_move/models/mrp_production.py
@@ -17,22 +17,20 @@ class MrpProduction(models.Model):
     @api.depends('move_raw_ids.display_quantity')
     def _compute_display_quantity(self):
         for production in self:
-            # If we consider the `move_raw_ids` field (moves that bring in raw materials)
-            # or `move_finished_ids` (moves that transfer finished goods)
             if production.move_raw_ids:
-                # Retrieve the first related `stock.move` record's `display_quantity`
-                # Customize as needed if there are multiple moves or a specific condition
                 stock_move = production.move_raw_ids.filtered(lambda m: m.display_quantity).sorted('date', reverse=True)[:1]
                 production.display_quantity = stock_move.display_quantity if stock_move else 0.0
+            else:
+                production.display_quantity = 0.0
 
     @api.depends('move_raw_ids.total_weighted_average')
     def _compute_weighted_average_pq(self):
         for production in self:
-            # If we consider the `move_raw_ids` field (moves that bring in raw materials)
-            # or `move_finished_ids` (moves that transfer finished goods)
             if production.move_raw_ids:
                 stock_move = production.move_raw_ids.filtered(lambda m: m.total_weighted_average).sorted('date', reverse=True)[:1]
                 production.weighted_average_pq = stock_move.total_weighted_average if stock_move else 0.0
+            else:
+                production.weighted_average_pq = 0.0
 
 class ChangeProductionQty(models.TransientModel):
     _inherit = 'change.production.qty'
@@ -42,7 +40,7 @@ class ChangeProductionQty(models.TransientModel):
             compute='_compute_product_qty',
             digits='Product Unit of Measure', required=True)
 
-    # overrides the 
+    # overrides the
     @api.depends_context('active_id')
     def _compute_product_qty(self):
         # Access the production order using the active_id from the context
@@ -52,4 +50,4 @@ class ChangeProductionQty(models.TransientModel):
                 production = self.env['mrp.production'].browse(production_id)
                 record.product_qty = production.display_quantity
             else:
-                record.product_qty = 10.0
+                record.product_qty = 0.0
