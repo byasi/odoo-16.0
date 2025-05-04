5e6668dcad4618dee38a44b0e6940a81343cdbe0
byasi <solomonbyasi@gmail.com>
2025-05-04 16:58:16 +0300

    create sales report

--- addons/sales_move/models/__init__.py
+++ addons/sales_move/models/__init__.py
@@ -1,12 +1,12 @@
-
-
-from . import account_payment
-from . import config_settings
 from . import purchase
-from . import purchase_config
+from . import account_payment
 from . import stock_move
-from . import stock_picking
 from . import mrp_production
+from . import sale_order_line
 from . import stock_move_line
 from . import stock_lot
-from .import sale_order_line
+from . import config_settings
+from . import stock_picking
+from . import purchase_config
+from . import customer_stock_report
+from . import account_move
--- addons/sales_move/wizard/__init__.py
+++ addons/sales_move/wizard/__init__.py
@@ -1,1 +1,2 @@
 from . import set_unfixed_price
+from . import set_opening_stock
